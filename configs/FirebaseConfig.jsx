8326601b020a5dae53ba479896769bf75effad4f
asifshaikh <shaikha015@gmail.com>
2025-01-04 21:21:50 +0530

    fix: remove dotenv integration and hardcode Firebase API key

--- configs/FirebaseConfig.jsx
+++ configs/FirebaseConfig.jsx
@@ -1,15 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import dotenv from 'dotenv';
-dotenv.config();
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: 'AIzaSyD5eeyecdJDx8Vx2K0IqSL9jxl_kfev6TM',
   authDomain: 'meditrack-b11da.firebaseapp.com',
   projectId: 'meditrack-b11da',
   storageBucket: 'meditrack-b11da.firebasestorage.app',
