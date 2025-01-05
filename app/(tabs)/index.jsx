044106169418d15e244ed3c42aa797a71e06bf10
asifshaikh <shaikha015@gmail.com>
2025-01-05 16:26:05 +0530

    feat: enhance logout functionality to clear local storage and redirect to login

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,14 +1,21 @@
 import { View, Text, Button } from 'react-native';
 import React from 'react';
-import { Redirect } from 'expo-router';
+import { Redirect, useRouter } from 'expo-router';
 import { signOut } from 'firebase/auth';
 import { auth } from '../../configs/FirebaseConfig';
+import { removeLocalStorage } from '../../service/Storage';
 
 const HomeScreen = () => {
+  const router = useRouter();
+  const handleSignOut = async () => {
+    await removeLocalStorage();
+    signOut(auth);
+    router.push('login');
+  };
   return (
     <View>
       <Text>HomeScreen</Text>
-      <Button title='Logout' onPress={() => signOut(auth)} />
+      <Button title='Logout' onPress={() => handleSignOut()} />
     </View>
   );
 };
